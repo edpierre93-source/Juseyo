297fade5c0bad969bf4e49d50dfe7ca4eddaf159
edpierre93-source <edpierre93@gmail.com>
2026-06-11 17:22:48 +0200

    modif js menu burger

--- burger.js
+++ burger.js
@@ -1,2 +1,16 @@
 const body = document.querySelector("body")
-const nav = document.querySelector(".header")
+const nav = document.querySelector("nav")
+const sidebarOpen = document.querySelector(".sidebar-open")
+const sidebarClose = document.querySelector(".sidebar-close")
+
+// js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active")
+})
+body.addEventListener('click', e => {
+    let clickedElm = e.target
+
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+    nav.classList.remove("active")
+    }
+})
